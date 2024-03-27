dfb9316759045b039dc18c13ab9c373800f01949
Affansyah Hanan W <afanhanan10@gmail.com>
2024-03-27 17:53:42 +0700

    Make Search Packet Page - Fix ESLint

--- app/(landingpage)/paket/SearchContent.tsx
+++ app/(landingpage)/paket/SearchContent.tsx
@@ -11,7 +11,7 @@ export default async function SearchContent() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
       {data.map((paket_umroh: any, index: number) => (
-        <PacketCard data={paket_umroh} index={index} />
+        <PacketCard data={paket_umroh} index={index} key={index}/>
       ))}
     </div>
   );
--- app/(landingpage)/paket/[slug]/UmrohPackage.tsx
+++ app/(landingpage)/paket/[slug]/UmrohPackage.tsx
@@ -7,7 +7,7 @@ export default function UmrohPackage({ data }: { data: any }) {
   return (
     <div className="grid sm:grid-cols-1 gap-3 my-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {paket_umroh?.map((paket_umroh: any, index: number) => (
-        <PacketCard data={paket_umroh} index={index} />
+        <PacketCard data={paket_umroh} index={index} key={index}/>
       ))}
     </div>
   );
