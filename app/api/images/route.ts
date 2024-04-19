35e90683935aad36deb973516456cecc123ddbf2
Affansyah Hanan W <afanhanan10@gmail.com>
2024-04-19 16:23:20 +0700

    Make a Verification System && Update Account:Delete File

--- app/api/images/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  const res = await fetch('http://localhost/storage/profiles/01hvqf3adtctrw3nbsd0ak01jj/617c823a243c06508734fecafca1d9d7.jpg');
-
-  const result = await res.blob();
-  return Response.json({
-    tes: 'opke'
-  })
-}
