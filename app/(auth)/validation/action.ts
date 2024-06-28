56ba0de7224c37a4a9cc7693b029c76a6e7bd529
LostInLost <afanhanan10@gmail.com>
2024-06-28 21:55:28 +0700

    Stable Deployment#3

--- app/(auth)/validation/action.ts
+++ app/(auth)/validation/action.ts
@@ -1,17 +1,21 @@
-'use server'
+"use server";
 
 import { newApiFetch } from "@/lib/Fetcher";
 import { getLaravelToken } from "@/lib/Handling/userSession";
+import { redirect } from "next/navigation";
 
 export async function getUserComparison() {
-    const token = await getLaravelToken();
-  
-    const res = await newApiFetch({
-      url: "/api/profile/comparison",
-      token: token,
-    });
-  
-    const result = await res.json();
+  const token = await getLaravelToken();
 
-    return result;
-  }
+  const res = await newApiFetch({
+    url: "/api/profile/comparison",
+    token: token,
+  });
+
+  const result = await res.json();
+  return result;
+}
+
+export async function redirectTo(string: string) {
+  redirect(string);
+}
